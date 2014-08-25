709a0458c5010bdbdf8682ea654467a3de55168d
Brachamul <barnaby.brachamul@gmail.com>
2014-08-25 19:23:09 +0200

    Enabled register, login, logout, added nav bar
    Used rango code heavily, imported icomoon symbols

--- profit/urls.py
+++ profit/urls.py
@@ -7,15 +7,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'signups.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	# Examples:
+	url(r'^$', 'signups.views.home', name='home'),
+	url(r'^u/', include('profiles.urls'), name='profiles'),
+	url(r'^admin/', include(admin.site.urls)),
+#	url(r'^blog/', include('blog.urls'), name='blog'),
+#	url(r'^$', RedirectView.as_view(permanent=False, url='/blog/')),
 )
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.STATIC_URL,
+						  document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL,
+						  document_root=settings.MEDIA_ROOT)
