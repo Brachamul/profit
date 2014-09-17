61be9262447e54a18c742e0bd5677f9ea1a99d9f
Brachamul <barnaby.brachamul@gmail.com>
2014-09-17 11:51:47 +0200

    minor update

---
+++ assets/blargh.py
@@ -0,0 +1,13 @@
+slot_details = [{
+    'number' : 0,
+    'name' : 'no name',
+    'owner' : 0,
+    'description' : 'no description'
+    'illustration' : 'placeholder.png',
+    'shape' : '30,0,61,15,31,31,0,16',
+    'longitude' : 0,
+    'latitude' : 0,
+    'vertical-offset' : 0,
+    'horizontal-offset' : 0,
+    'buttons' : '<a href="build"></a>',
+} for k in range(list_of_all_slots)]
